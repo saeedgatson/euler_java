6c9914dfdb03e9841d40bdf1e71c224a717711a6
Saeed Gatson <saeedgatson@gmail.com>
2015-06-04 16:07:48 -0700

    Add Problem 2 Final Solution

--- level_01/EvenFibonacciNumbers.java
+++ level_01/EvenFibonacciNumbers.java
@@ -1,18 +1,30 @@
 class EvenFibonacciNumbers {
-  public int sum;
+  private static int sum = 0;
+  private static int maxValue = 4000000;
+
   public static void main (String[] args) {
-    for(int i = 2; i < 12; i++) {
-      System.out.println(fib(i));
+    int iterator = 0;
+    int sequenceValue = 0;
+
+    while (sequenceValue < maxValue) {
+      sequenceValue = fib(iterator);
+      if (sequenceValue % 2 == 0) {
+        sum = sum + sequenceValue;
+      }
+      iterator++;
     }
+
+    System.out.println(sum);
   }
 
-  public static int fib(int value) {
-    if (value == 0) {
+  public static int fib(int number) {
+    if (number == 0) {
       return 0;
     }
-    else if (value == 1) {
+    else if (number == 1) {
       return 1;
     }
-    return fib(value - 1) + fib(value - 2);
+
+    return fib(number - 1) + fib(number - 2);
   }
 }
