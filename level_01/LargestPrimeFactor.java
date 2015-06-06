4a994ae40bdaad8c43f726678300bcba93c7c1bc
Saeed Gatson <saeedgatson@gmail.com>
2015-06-05 20:40:52 -0700

    Add Solution For Problem 3

--- level_01/LargestPrimeFactor.java
+++ level_01/LargestPrimeFactor.java
@@ -1,10 +1,24 @@
 public class LargestPrimeFactor {
   public static void main (String [] args) {
     LargestPrimeFactor primeFactor = new LargestPrimeFactor();
-    System.out.println(primeFactor.largestPrimeGivenList(13195));
+    System.out.println("Largest Prime Factor of 600851475143 is: " + primeFactor.largestPrimeFactor(600851475143L));
   }
 
-  private int largestPrimeGivenList(int number) {
+  private int largestPrimeFactor(long number) {
+    long n = number;
+    int highest = 2;
+    for (int i = 2; i <= n; i++) {
+      while (n % i == 0) {
+        if(highest < i) {
+          highest = i;
+        }
+        n /= i;
+      }
+    }
+    return highest;
+  }
+
+  private int largestPrimeFactorGivenList(int number) {
     int[] primeList = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71};
     int highest = 2;
 
