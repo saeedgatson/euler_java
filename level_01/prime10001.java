b0102372c5342cab2d3495f63e8ef3f47b5dbe77
Saeed Gatson <saeedgatson@gmail.com>
2015-06-09 16:32:01 -0700

    Add Solution For Problem 7

---
+++ level_01/prime10001.java
@@ -0,0 +1,28 @@
+import java.lang.Math;
+
+class prime10001 {
+  public static void main (String[] args) {
+    prime10001 primes = new prime10001();
+    int value = 2;
+
+    for(int i = 2, j = 0; j < 10001; i++ ) {
+      if(primes.isPrime(i)) {
+        value = i;
+        j++;
+      }
+    }
+
+    System.out.println("The 10001 prime number is : " + (value));
+  }
+
+  private boolean isPrime(int number) {
+    Double rootDbl = Math.sqrt(number);
+    int root = rootDbl.intValue();
+    for(int i = 2; i <= root; i++) {
+      if(number % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
