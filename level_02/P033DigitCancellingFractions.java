de6237494d696a6940c2f148a63e117fd1786465
Saeed Gatson <saeedgatson@gmail.com>
2015-08-27 18:49:06 -0700

    Add Problem 33 Solution.

--- level_02/P033DigitCancellingFractions.java
+++ level_02/P033DigitCancellingFractions.java
@@ -8,49 +8,30 @@ public class P033DigitCancellingFractions {
   public static int numeratorProduct = 0;
   public static int denominatorProduct = 0;
 
-  public static int curiousDivide(int n, int d) {
-    String n_str = String.valueOf(n);
-    String d_str = String.valueOf(d);
-    String str = n_str + d_str;
-    char removeValue = '\0';
-
-    for (char c : str.toCharArray()) {
-      System.out.println("char " + c);
-      int count = str.length() - str.replace(c, '\0').length();
-      System.out.println("count " + count);
-      if(count == 2)
-        removeValue = c;
+  private static int gcd(int a, int b) {
+    while (b > 0) {
+        int temp = b;
+        b = a % b; // % is remainder
+        a = temp;
     }
-
-    System.out.println("remove " + removeValue);
-
-    n_str = n_str.replace(removeValue, '\0');
-    d_str = d_str.replace(removeValue, '\0');
-    System.out.println("n_str " + n_str);
-    System.out.println("d_str " + d_str);
-
-    return Integer.parseInt(n_str) / Integer.parseInt(d_str);
-  }
-
-  public static void doWork(int n, int d) {
-    if(n % 10 == 0 || d % 10 == 0) return;
-    int c1 = n / d;
-    int c2 = curiousDivide(n, d);
-    System.out.println(c1);
-    System.out.println(c2);
-    if(c1 == c2) {
-      numeratorProduct *= n;
-      denominatorProduct *= d;
-    }
+    return a;
   }
 
   public static void main(String[] args) {
-    for(int n = 10; n <= 98; n++) {
-      for(int d = n + 1; d <= 99; d++) {
-        doWork(n, d);
-      }
-    }
-    System.out.println(denominatorProduct);
-    System.out.println(numeratorProduct);
+    int numerator = 1;
+		int denomrator = 1;
+		for (int d = 10; d < 100; d++) {
+			for (int n = 10; n < d; n++) {
+        int n1 = n % 10;
+        int n2 = n / 10;
+				int d1 = d % 10;
+        int d2 = d / 10;
+				if (n2 == d1 && n1 * d == n * d2 || n1 == d2 && n2 * d == n * d1) {
+					numerator *= n;
+					denomrator *= d;
+				}
+			}
+		}
+		System.out.println(Integer.toString(denomrator / gcd(numerator, denomrator)));
   }
 }
